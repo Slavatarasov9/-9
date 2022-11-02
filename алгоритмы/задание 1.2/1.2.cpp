512071480a9f63e83c4beeb325e19fba449f4416
Slavatarasov9 <117200512+Slavatarasov9@users.noreply.github.com>
2022-11-02 23:36:45 +0500

    Add files via upload

---
+++ алгоритмы/задание 1.2/1.2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace
+int main()
+{
+    float a, b;
+    
+    cin >> a >> b;
+    
+    if (b == 0)
+    {
+        cout << "На ноль делить нельзя!" <<
+    }
+    else 
+    {
+        cout << a / b << endl;
+    }
+    
+}
