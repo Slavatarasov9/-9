5f640246354e44837cb5d57c799c5b50aeaeb86b
Slavatarasov9 <117200512+Slavatarasov9@users.noreply.github.com>
2022-11-22 23:14:14 +0500

    Add files via upload

---
+++ алгоритмы/задание 2.11/2.11.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, tmp;
+bool plusFirst = false; // flag
+bool minusFirst = false; // flag
+
+cout << "Введите длину последовательности:" << endl;
+cin >> n;
+
+for (int i = 1; i <= n; i++)
+{
+    cin >> tmp;
+    if (!plusFirst && !minusFirst)
+    {
+        if (tmp > 0) { plusFirst = true; }
+        else if (tmp < 0) { minusFirst = true; }
+    }
+}
+
+if (plusFirst)
+{
+    cout << "Сначала ввели положительное число";
+}
+else if (minusFirst)
+{
+    cout << "Сначала ввели отрицательное число";
+}
+else
+{
+    cout << "Все были нулевыми";
+}
+cout << endl;
+
+}
