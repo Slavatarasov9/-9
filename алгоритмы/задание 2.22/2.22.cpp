86f45375ef0dbe3448d65af0f559e4197d13ad03
Slavatarasov9 <117200512+Slavatarasov9@users.noreply.github.com>
2022-12-20 20:53:12 +0500

    Add files via upload

---
+++ алгоритмы/задание 2.22/2.22.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    float n, a, max;
+    int i = 2;
+    int numFirst = 1;
+    int numLast = 1;
+
+    cin >> n;
+
+    max = sin(n + 1/n);
+
+    while (i <= n)
+    {
+        a = sin(n + 1/n);
+        if (a > max)
+        {
+            max = a;
+            numFirst = i;
+            numLast = i;
+        }
+        else if (a == max)
+        {
+            numLast = i;
+        }
+        i++;
+    }
+    cout << "Максимальный элемент: " << max << endl;
+    cout << "№ Первого элемента с этим значением: " << numFirst << endl;
+    cout << "№ последнего элемента с этим значением: " << numLast << endl;
+}
+
